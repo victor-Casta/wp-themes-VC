fd110b406998d57bb6fd5320313181b0fd02c50f
Victor Castañeda <victorarroyavecastaneda@gmail.com>
2024-09-24 18:02:34 -0500

    update nav menu

--- landing-page/header.php
+++ landing-page/header.php
@@ -24,13 +24,7 @@
         <img class="menu-mobile-icon" src="<?php echo get_template_directory_uri(); ?>/assets/icons/burguerMenu.svg" alt="burguer menu icon">
       <nav class="mobile-menu inactive">
         <img src="<?php echo get_template_directory_uri() ?>/assets/icons/close.svg" class="close-icon" alt="close icon">
-        <ul>
-          <li><a href="">about us</a></li>
-          <li><a href="">services</a></li>
-          <li><a href="">use cases</a></li>
-          <li><a href="">pricing</a></li>
-          <li><a href="/Blog">blog</a></li>
-        </ul>
+        <?php wp_nav_menu(); ?>
         <a href="/" class="button__header">request a quote</a>
       </nav>
     </section>
